d4dce38d9d5e3a7fd1731da2e191952d8b85d3ea
NikitaPO <gigzim345@gmail.com>
2020-12-29 03:49:57 +0300

    products block and start of popular block

---
+++ app/js/index.js
@@ -0,0 +1,7 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+
+// init Swiper:
+const swiper = new Swiper();
